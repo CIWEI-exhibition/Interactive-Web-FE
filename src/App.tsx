cfb23971f91b7c4f914bf290a6c538273dbdb5f9
Subin <80036437+hansb1023@users.noreply.github.com>
2026-06-08 21:37:50 +0900

    [refactor] 메인 페이지 entrance.html로 리다이렉트, NavBar React 전용 페이지에만 표시

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,21 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import MainPage from './pages/MainPage'
 import ArtistPage from './pages/ArtistPage'
 import ArtworkDetailPage from './pages/ArtworkDetailPage'
-import GoodsPage from './pages/GoodsPage'
-import MemoPage from './pages/MemoPage'
 
 function App() {
+  const { pathname } = useLocation()
+  const showNav = pathname !== '/'
+
   return (
     <>
-      <NavBar />
-      <main className="pt-16">
+      {showNav && <NavBar />}
+      <main className={showNav ? 'pt-16' : ''}>
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/artist/:id" element={<ArtistPage />} />
           <Route path="/artwork/:id" element={<ArtworkDetailPage />} />
-          <Route path="/goods" element={<GoodsPage />} />
-          <Route path="/memo" element={<MemoPage />} />
         </Routes>
       </main>
     </>
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,16 +1,8 @@
-import { motion } from 'framer-motion'
+import { useEffect } from 'react'
 
 export default function MainPage() {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <motion.h1
-        className="text-6xl font-thin tracking-[0.3em] text-white"
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 1.2, ease: 'easeOut' }}
-      >
-        AQUAURORE
-      </motion.h1>
-    </div>
-  )
+  useEffect(() => {
+    window.location.replace('/Main_Page/entrance.html')
+  }, [])
+  return null
 }
